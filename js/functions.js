24aaf4ca3330b5e3eb8699c6a742fa8e9f914e0e
Jeff Neumann <jneumann@denverpost.com>
2016-03-31 14:40:48 -0600

    2012 slideshow

--- js/functions.js
+++ js/functions.js
@@ -55,8 +55,8 @@ function revealSlides(galleries) {
                 centerMode: true,
                 centerPadding: '15%',
                 slidesToShow: 1,
-                prevArrow: '<button type="button" class="slick-prev"><span>&lt;</span></button>',
-                nextArrow: '<button type="button" class="slick-next"><span>&gt;</span></button>',
+                prevArrow: '<button type="button" class="slick-prev"><span>&lsaquo;</span></button>',
+                nextArrow: '<button type="button" class="slick-next"><span>&rsaquo;</span></button>',
                 responsive: [{
                     breakpoint: 800,
                     settings: {
